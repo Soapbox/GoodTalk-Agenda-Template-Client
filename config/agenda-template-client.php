79466d70c8deb98768cce666b8f711e17e9669b2
JaskirtPooni <jaskirt@soapboxhq.com>
2020-06-02 10:48:09 -0400

    wip

--- config/agenda-template-client.php
+++ config/agenda-template-client.php
@@ -9,14 +9,14 @@ return [
     'signed-requests' => [
         'algorithm' => env('ATC_SIGNED_REQUEST_ALGORITHM', 'sha256'),
         'cache-prefix' => env('ATC_SIGNED_REQUEST_CACHE_PREFIX', 'signed-requests'),
-    ],
-    'headers' => [
-        'signature' => env('ATC_SIGNED_REQUEST_SIGNATURE_HEADER', 'X-Signature'),
-        'algorithm' => env('ATC_SIGNED_REQUEST_ALGORITHM_HEADER', 'X-Signature-Algorithm'),
-    ],
-    'key' => env('ATC_SIGNED_REQUEST_KEY', 'customKey'),
-    'request-replay' => [
-        'allow' => env('ATC_SIGNED_REQUEST_ALLOW_REPLAYS', false),
-        'tolerance' => env('ATC_SIGNED_REQUEST_TOLERANCE_SECONDS', 30),
+        'headers' => [
+          'signature' => env('ATC_SIGNED_REQUEST_SIGNATURE_HEADER', 'X-Signature'),
+          'algorithm' => env('ATC_SIGNED_REQUEST_ALGORITHM_HEADER', 'X-Signature-Algorithm'),
+        ],
+        'key' => env('ATC_SIGNED_REQUEST_KEY', 'customKey'),
+        'request-replay' => [
+            'allow' => env('ATC_SIGNED_REQUEST_ALLOW_REPLAYS', false),
+            'tolerance' => env('ATC_SIGNED_REQUEST_TOLERANCE_SECONDS', 30),
+        ],
     ],
 ];
